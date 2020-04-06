c4f8bf601481f64cbc31bb89dae1b3ebeaf05cfd
Marcel Benders <marcel.benders@icloud.com>
2020-04-06 22:29:29 +0200

    Update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 #See https://aka.ms/containerfastmode to understand how Visual Studio uses this Dockerfile to build your images for faster debugging.
 
 FROM mcr.microsoft.com/dotnet/core/aspnet:3.1-buster-slim AS base
-WORKDIR /app
 EXPOSE 5000
 
 FROM mcr.microsoft.com/dotnet/core/sdk:3.1-buster AS build
@@ -15,24 +14,29 @@ RUN apt-get install curl gnupg -yq
 RUN curl -sL https://deb.nodesource.com/setup_13.x | bash -
 RUN apt-get install -y nodejs
 
-#COPY ["Deploy_O_Mat.API/Deploy_O_Mat.API.csproj", "Deploy_O_Mat.API/"]
-COPY . .
-#COPY ["NuGet.config", "Deploy_O_Mat.API/"]
-RUN ls
+# Copy only API for restore packages
+COPY ["Deploy_O_Mat.API/Deploy_O_Mat.API.csproj", "Deploy_O_Mat.API/"]
+
+# Restore packages
 RUN dotnet restore "Deploy_O_Mat.API/Deploy_O_Mat.API.csproj"
 
-#WORKDIR "/src/Deploy_O_Mat.API"
-WORKDIR "/src/deploy-o-mat"
-RUN npm install
-RUN npm run build
-RUN ls
+# Copy rest of the Project
+COPY . .
+
+# Build API
 WORKDIR "/src/Deploy_O_Mat.API"
 RUN dotnet build "Deploy_O_Mat.API.csproj" -c Release -o /app/build
 
+# Restore and Build SPA
+WORKDIR "/src/deploy-o-mat"
+RUN ls
+RUN npm install
+RUN npm run build
+
+# Publish
+WORKDIR "/src/Deploy_O_Mat.API"
 FROM build AS publish
 RUN dotnet publish "Deploy_O_Mat.API.csproj" -c Release -o /app/publish
-
-
 
 FROM base AS final
 WORKDIR /app
--- deploy-o-mat/src/features/build-status/BuildStatus.tsx
+++ deploy-o-mat/src/features/build-status/BuildStatus.tsx
@@ -3,12 +3,35 @@ import { Container } from 'semantic-ui-react';
 
 const BuildStatus: React.FC = () => {
     return (
-        <Container style={{ marginTop: '4em' }}  >
-            <img src={"https://github.com/Marcel-B/Deploy-O-Mat/workflows/Publish%20Docker/badge.svg"} style={{ height: '20px' }} />
-            <img src={"https://github.com/Marcel-B/Deploy-O-Mat/workflows/.NET%20Core/badge.svg"} style={{ height: '20px', marginLeft: '4px' }} />
-            <img src={"https://github.com/Marcel-B/Deploy-O-Mat/workflows/Docker%20Image%20CI/badge.svg"} style={{ height: '20px', marginLeft: '4px' }} />
+        <Container style={{ marginTop: '4em' }}>
+            <img
+                src={
+                    'https://github.com/Marcel-B/Deploy-O-Mat/workflows/Publish%20Docker/badge.svg'
+                }
+                style={{ height: '20px' }}
+            />
+            <img
+                src={
+                    'https://github.com/Marcel-B/Deploy-O-Mat/workflows/.NET%20Core/badge.svg'
+                }
+                style={{ height: '20px', marginLeft: '4px' }}
+            />
+            <img
+                src={
+                    'https://github.com/Marcel-B/Deploy-O-Mat/workflows/Docker%20Image%20CI/badge.svg'
+                }
+                style={{ height: '20px', marginLeft: '4px' }}
+            />
+            <span>
+                <img
+                    src={
+                        'https://github.com/Marcel-B/Utilities/workflows/.NET%20Core/badge.svg'
+                    }
+                    style={{ height: '20px', marginLeft: '4px' }}
+                />
+            </span>
         </Container>
-    )
+    );
 };
 
 export default BuildStatus;

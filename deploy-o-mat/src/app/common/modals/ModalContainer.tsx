e709eaf41c2836be1b35bf1fcd5a6dd9ce9008da
Marcel Benders <marcel.benders@icloud.com>
2020-04-16 12:50:47 +0200

    Add Modal Part 1

---
+++ deploy-o-mat/src/app/common/modals/ModalContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Modal } from 'semantic-ui-react';
+
+const ModelContainer = () => {
+    return (
+        <Modal>
+            <Modal.Content>Our component will go here</Modal.Content>
+        </Modal>
+    );
+};
+
+export default ModelContainer;

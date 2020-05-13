c62da3fe449aadfdb33e38174dd4d6de9ba45191
Marcel <marcel.benders@b-velop.com>
2020-05-13 23:57:59 +0200

    (dev) Add Logging

--- Deploy-O-Mat.Web.Application/DockerStack/Start.cs
+++ Deploy-O-Mat.Web.Application/DockerStack/Start.cs
@@ -5,6 +5,7 @@ using com.b_velop.Deploy_O_Mat.Web.Application.Contracts;
 using com.b_velop.Deploy_O_Mat.Web.Persistence.Context;
 using FluentValidation;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace com.b_velop.Deploy_O_Mat.Web.Application.DockerStack
 {
@@ -25,11 +26,14 @@ namespace com.b_velop.Deploy_O_Mat.Web.Application.DockerStack
 
         public class Handler : IRequestHandler<Start.Command>
         {
+            private readonly ILogger<Handler> _logger;
             private readonly IDockerStackService _dockerStackService;
 
             public Handler(
+                ILogger<Handler> logger,
                 IDockerStackService dockerStackService)
             {
+                _logger = logger;
                 _dockerStackService = dockerStackService;
             }
 
@@ -37,6 +41,7 @@ namespace com.b_velop.Deploy_O_Mat.Web.Application.DockerStack
                 Start.Command request,
                 CancellationToken cancellationToken)
             {
+                _logger.LogInformation($"Stop Stack called");
                 await _dockerStackService.StartStack(request.Id, cancellationToken);
                 return Unit.Value;
             }
--- Deploy-O-Mat.Web.Application/DockerStack/Stop.cs
+++ Deploy-O-Mat.Web.Application/DockerStack/Stop.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using com.b_velop.Deploy_O_Mat.Web.Application.Contracts;
 using FluentValidation;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace com.b_velop.Deploy_O_Mat.Web.Application.DockerStack
 {
@@ -25,17 +26,21 @@ namespace com.b_velop.Deploy_O_Mat.Web.Application.DockerStack
         public class Handler : IRequestHandler<Stop.Command>
         {
             private readonly IDockerStackService _dockerStackService;
+            private readonly ILogger<Handler> _logger;
 
             public Handler(
-                IDockerStackService dockerStackService)
+                IDockerStackService dockerStackService,
+                ILogger<Handler> logger)
             {
                 _dockerStackService = dockerStackService;
+                _logger = logger;
             }
 
             public async Task<Unit> Handle(
                 Stop.Command request,
                 CancellationToken cancellationToken)
             {
+                _logger.LogInformation($"Stop Stack called");
                 await _dockerStackService.StopStack(request.Id, cancellationToken);
                 return Unit.Value;
             }
